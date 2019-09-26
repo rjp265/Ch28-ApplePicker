97bc476af27956e4c654a8f6656d1669118e33b0
Robert Perez <49209439+rjp265@users.noreply.github.com>
2019-09-26 19:54:50 -0400

    Everything is done, except the score and highscore

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 public class Basket : MonoBehaviour
 {
+    public GUIText scoreGT;
+
     void Update()
     {
         Vector3 mousePos2D = Input.mousePosition; // 1
@@ -15,6 +17,16 @@ public class Basket : MonoBehaviour
         this.transform.position = pos;
     }
 
+    void Start()
+    {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter"); // 2
+                                                              // Get the GUIText Component of that GameObject
+        scoreGT = scoreGO.GetComponent<GUIText>(); // 3
+                                                   // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
+
     void OnCollisionEnter(Collision coll)
     { // 2
       // Find out what hit this basket
@@ -23,6 +35,20 @@ public class Basket : MonoBehaviour
         { // 4
             Destroy(collidedWith);
         }
+
+        // Parse the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text); // 4
+                                             // Add points for catching the apple
+        score += 100;
+        // Convert the score back to a string and display it
+        scoreGT.text = score.ToString();
+
+        // Track the high score
+        if (score > HighScore.score)
+        {
+            HighScore.score = score;
+        }
+
     }
 
 }
---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+    void Awake()
+    { // 1
+      // If the ApplePickerHighScore already exists, read it
+        if (PlayerPrefs.HasKey("ApplePickerHighScore"))
+        { // 2
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        // Assign the high score to ApplePickerHighScore
+        PlayerPrefs.SetInt("ApplePickerHighScore", score); // 3
+    }
+    void Update()
+    {
+        GUIText gt = this.GetComponent<GUIText>();
+        gt.text = "High Score: " + score;
+        // Update ApplePickerHighScore in PlayerPrefs if necessary
+        if (score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        { // 4
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+    }
+}
