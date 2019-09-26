d585cac2b1869d4e420322d62a81c8c26052d313
Robert Perez <49209439+rjp265@users.noreply.github.com>
2019-09-26 18:38:48 -0400

    Backet catches apples

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Basket : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos2D = Input.mousePosition; // 1
+                                                  // The Camera's z position sets the how far to push the mouse into 3D
+        mousePos2D.z = -Camera.main.transform.position.z; // 2
+                                                          // Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D); // 3
+                                                                         // Move the x position of this Basket to the x position of the Mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    { // 2
+      // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject; // 3
+        if (collidedWith.tag == "Apple")
+        { // 4
+            Destroy(collidedWith);
+        }
+    }
+
+}
